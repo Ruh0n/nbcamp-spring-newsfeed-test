2121c0a36adc72bf59524392f8213d1058c8bea5
ruh0n <wns8256@naver.com>
2024-02-19 12:10:47 +0900

    :heavy_plus_sign: chore(test): add jacoco and fixture-monkey

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/global/config/LombokConfig.java
@@ -0,0 +1,10 @@
+package com.ptjcoding.nbcampspringnewsfeed.global.config;
+
+import lombok.Generated;
+import org.springframework.context.annotation.Configuration;
+
+@Generated
+@Configuration
+public class LombokConfig {
+
+}
--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/global/config/WebConfig.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/global/config/WebConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 @RequiredArgsConstructor
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final JwtProvider jwtProvider;
 
