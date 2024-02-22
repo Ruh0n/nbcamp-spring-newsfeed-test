c97d044f7da6f0fac4455d850ba96cf866b92581
ruh0n <wns8256@naver.com>
2024-02-22 17:31:33 +0900

    :rocket: feat(member): Implement @AllArgsConstructor for DTOs

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/dto/SignupRequestDto.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/dto/SignupRequestDto.java
@@ -3,11 +3,15 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.member.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequestDto {
 
   @NotBlank
